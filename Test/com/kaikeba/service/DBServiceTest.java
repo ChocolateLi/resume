dfbeb35e1f15da0f147fc43c93f8470fb9ddaf53
ChocolateLi <48371261+ChocolateLi@users.noreply.github.com>
2021-01-27 21:56:31 +0800

    Add files via upload
    first commit

---
+++ Test/com/kaikeba/service/DBServiceTest.java
@@ -0,0 +1,15 @@
+package com.kaikeba.service;
+
+import com.kaikeba.bean.User;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DBServiceTest {
+
+    @Test
+    public void userInsert(){
+        User user = new User("1",18,"1","1","1","1","1","1","1","1","1");
+        DBService.insertUser(user);
+    }
+}
